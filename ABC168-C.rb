d2c5a5c0e1acd8aef2c00855f300e25c75056a75
Naohisa Yokoyama <nhsykym@gmail.com>
2020-05-18 16:57:04 +0900

    Fix

--- ABC168-C.rb
+++ ABC168-C.rb
@@ -4,8 +4,6 @@ PI = Math::PI
 H_angle = H / 12.0 + (M / 60.0) / 12.0
 M_angle = M / 60.0
 
-puts H_angle
-puts M_angle
 Angle = ((M_angle - H_angle).abs) * PI * 2
 
 puts Math.sqrt((A ** 2 + B ** 2) - (2 * A * B) * Math.cos(Angle))
